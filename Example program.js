da9f0aba5163b0f23f2913cc8413a79bad83cc65
Chethan A C <acchethan15@gmail.com>
2025-01-02 23:27:09 +0530

    Create Example program.js

---
+++ Example program.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+       <h1>My favourite food</h1>
+          <ul>
+              <li>Fried rice</li>
+              <li>Roti curry</li>
+          </ul>
+  </div>,
+document.getElementById("root")
+);
