b0e5f1dad6465e93bc4673b4100abd53cb80a7e4
Chethan A C <acchethan15@gmail.com>
2025-01-04 23:20:29 +0530

    Create Component.js

---
+++ Component.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Heading() {
+  return <h1>Favourite Food</h1>;
+}
+
+ReactDom.render{
+<div>
+  <Heading/>
+  <ul>
+      <li>Fried rice </li>
+      <li>Roti curry </li>
+  </ul>
+</div>,
+  document.getElementById("root")
+  );
