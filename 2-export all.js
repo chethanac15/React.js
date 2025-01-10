57e70affb48a3c49bb7e2f88cf4975d39ecb5982
Chethan A C <acchethan15@gmail.com>
2025-01-10 22:14:20 +0530

    Update 2-import&export all.js

--- 2-export all.js
+++ 2-export all.js
@@ -8,3 +8,17 @@ function triplePi() {
 }
 export default pi;
 export {doublePi,triplePi}
+
+//in index.js
+
+import React from "react";
+import ReactDOM from "react-dom";
+import PI,{doublePi, triplePi} from "./math.js";
+ReactDOM.render (
+  <ul>
+  <li>(PI)</li>
+  <li>{doublePi()}</li>
+  <li>{triplePi()}</li>
+  </ul>,
+document.getElementById("root")
+);
