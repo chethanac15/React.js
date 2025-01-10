00065536125bcd95097ab0ac9255bc4b60b51ecd
Chethan A C <acchethan15@gmail.com>
2025-01-10 22:08:10 +0530

    Create 1-import&export.js

---
+++ 1-import&export.js
@@ -0,0 +1,15 @@
+//create new file math.js
+const pi=3.14;
+export default pi;
+
+//mive to index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import pi from "./math js";
+
+ReactDOM.render (
+  <ul>
+     <li>(pi)</li>
+  </ul>,
+document.getElementById("root")
+);
