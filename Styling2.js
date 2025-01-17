389354e7beac4262f0a5968b76d8ab32b72e3514
Chethan A C <acchethan15@gmail.com>
2025-01-17 23:43:36 +0530

    Create Styling2.js

---
+++ Styling2.js
@@ -0,0 +1,19 @@
+//index.html
+
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+       <h1 classname="heading">Topic</h1>
+       <p>Practicing react</p>
+  </div>,
+document.getElementById("root")
+);
+
+
+
+style.css
+heading {
+  color:blue;
+  font:18px;
+}
